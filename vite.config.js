cd350cb0a28bd8d57f4cf3c9710ab03b0fc0f2ff
Mike <maicolmartinezjd@unimagdalena.edu.co>
2025-01-06 11:46:46 -0500

    ajustes deploy

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/rick-and-morty-react/',
+  base: 'https://https://mikeOz27.github.io/rick-and-morty-react',
   // build: {
   //   outDir: 'build' // Cambia el nombre aquí a 'build' si lo prefieres
   // }
